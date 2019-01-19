9d9e58870cbd41d8526ddfb2df83fda6f511d33b
KaravolisL <lukejrkaravolis@gmail.com>
2019-01-19 18:01:31 -0500

    Functional way of viewing prizes

--- viewPrizeWindow.java
+++ viewPrizeWindow.java
@@ -21,14 +21,27 @@ public class viewPrizeWindow {
       public static void display() {
             window.setTitle("Prizes");
 
+            ObservableList<Prize> data = FXCollections.observableArrayList(Raffle.prizeInfo);
 
-            StackPane layout = new StackPane();
+            TableColumn prizeStatement = new TableColumn("Prize Description");
+            prizeStatement.setCellValueFactory(new PropertyValueFactory<>("statement"));
+            TableColumn prizeNumber = new TableColumn("Prize Number");
+            prizeNumber.setCellValueFactory(new PropertyValueFactory<>("number"));
 
+            table.getColumns().addAll(prizeNumber, prizeStatement);
+            table.setEditable(false);
 
+            table.setItems(data);
+
+            StackPane layout = new StackPane(table);
             Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
 		double screenHeight = bounds.getMaxY();
 		double screenWidth = bounds.getMaxX();
             scene = new Scene(layout, screenWidth/2, screenHeight/2);
+            prizeStatement.prefWidthProperty().bind(layout.widthProperty());
+            prizeNumber.prefWidthProperty().bind(layout.widthProperty());
+            prizeStatement.setMaxWidth(7*screenWidth/8);
+            prizeNumber.setMaxWidth(screenWidth/8);
             window.setScene(scene);
             window.setResizable(false);
             // Setting the window's icon
