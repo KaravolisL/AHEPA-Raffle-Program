bc47cc7159395c60f98a75fab0c1f07901490d4d
KaravolisL <lukejrkaravolis@gmail.com>
2019-02-02 16:01:13 -0500

    Added buttons to editPrizeAlertWindow

--- editPrizeAlertWindow.java
+++ editPrizeAlertWindow.java
@@ -21,18 +21,24 @@ public class editPrizeAlertWindow {
       private Label displayDuration = new Label("Display Duration (in seconds): ");
       private TextField durationInput = new TextField("" + Raffle.waitTime);
       private Text warning = new Text();
+      private Button confirmButton = new Button("Confirm");
+      private Button cancelButton = new Button("Cancel");
+      private Integer newWaitTime = Raffle.waitTime;
 
-      public void display() {
+      public editPrizeAlertWindow() {
             window.setTitle("Edit Prize Alerts");
 
             layout.addRow(0, displayDuration, durationInput);
             layout.add(warning, 0, 1, 2, 1);
+            layout.addRow(2, confirmButton, cancelButton);
+            GridPane.setHalignment(confirmButton, HPos.CENTER);
+            GridPane.setHalignment(cancelButton, HPos.CENTER);
             layout.setAlignment(Pos.CENTER);
 
             durationInput.textProperty().addListener(new ChangeListener<String>() {
                   public void changed(ObservableValue ov, String s, String s1) {
                         try {
-                              Raffle.waitTime = Integer.parseInt(s1);
+                              newWaitTime = Integer.parseInt(s1);
                               warning.setText("");
                         } catch (Exception e) {
                               warning.setText("Invalid input");
@@ -40,12 +46,23 @@ public class editPrizeAlertWindow {
                   }
             });
 
+            confirmButton.setOnAction(e -> {
+                  Raffle.waitTime = newWaitTime;
+                  window.close();
+            });
+
+            cancelButton.setOnAction(e -> window.close());
+
             scene = new Scene(layout, 400, 175);
             layout.prefWidthProperty().bind(scene.widthProperty());
             layout.prefHeightProperty().bind(scene.heightProperty());
             window.setScene(scene);
             // Setting the window's icon
             window.getIcons().add(new Image("Icon.jpg"));
+            window.initModality(Modality.APPLICATION_MODAL);;
+      }
+
+      public void display() {
             window.showAndWait();
       }
 
